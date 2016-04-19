bf11d2e55462f5206f58337fa25838c845ea91f6
MarkKeogh95 <keogher12@gmail.com>
2016-04-19 20:52:00 +0100

    Initial Version

---
+++ src/Snake.java
@@ -0,0 +1,20 @@
+import processing.core.*;
+
+public class Snake extends PApplet {
+
+	public void setup()
+	{
+		size(400,400);
+		background(0);
+	}
+	
+	public void draw()
+	{
+		
+	}
+	
+	public static void main(String args[]) 
+	{
+	    PApplet.main(new String[] { "--present", "Snake" });
+	}
+}
